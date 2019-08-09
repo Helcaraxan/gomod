c8bead1badf2a0793dec7f19f503ecca7e560538
Duco van Amstel <duco.vanamstel@gmail.com>
2019-08-09 20:13:56 +0100

    Replace custom teewriter with io.MultiWriter (#42)

--- lib/internal/util/exec.go
+++ lib/internal/util/exec.go
@@ -19,48 +19,28 @@ func RunCommand(logger *logrus.Logger, path string, cmd string, args ...string) 
 		}
 	}
 
-	stdoutWriter := &bufferedTeeWriter{logger: logger}
-	stderrWriter := &bufferedTeeWriter{logger: logger}
+	stdoutBuffer := &bytes.Buffer{}
+	stderrBuffer := &bytes.Buffer{}
 
 	execCmd := exec.Command(cmd, args...)
 	execCmd.Dir = path
-	execCmd.Stdout = stdoutWriter
-	execCmd.Stderr = stderrWriter
+	execCmd.Stdout = stdoutBuffer
+	execCmd.Stderr = stderrBuffer
 
 	if logger.GetLevel() >= logrus.DebugLevel {
-		stdoutWriter.printer = os.Stdout
+		execCmd.Stdout = io.MultiWriter(execCmd.Stdout, os.Stdout)
 	}
 	if logger.GetLevel() >= logrus.WarnLevel {
-		stderrWriter.printer = os.Stderr
+		execCmd.Stderr = io.MultiWriter(execCmd.Stderr, os.Stderr)
 	}
 
 	logger.Debugf("Running command '%s %s'.", execCmd.Path, strings.Join(execCmd.Args, " "))
 	err = execCmd.Run()
-	logger.Debugf("Content of stdout was: %s", stdoutWriter.buffer.Bytes())
-	logger.Debugf("Content of stderr was: %s", stderrWriter.buffer.Bytes())
+	logger.Debugf("Content of stdout was: %s", stdoutBuffer.Bytes())
+	logger.Debugf("Content of stderr was: %s", stderrBuffer.Bytes())
 	if err != nil {
 		logger.WithError(err).Errorf("'%s %s' exited with an error", execCmd.Path, strings.Join(execCmd.Args, " "))
-		return stdoutWriter.buffer.Bytes(), stderrWriter.buffer.Bytes(), fmt.Errorf("failed to run '%s %s: %s", cmd, strings.Join(args, " "), err)
+		return stdoutBuffer.Bytes(), stderrBuffer.Bytes(), fmt.Errorf("failed to run '%s %s: %s", cmd, strings.Join(args, " "), err)
 	}
-	return stdoutWriter.buffer.Bytes(), stderrWriter.buffer.Bytes(), nil
-}
-
-type bufferedTeeWriter struct {
-	logger  *logrus.Logger
-	buffer  bytes.Buffer
-	printer io.Writer
-}
-
-func (w *bufferedTeeWriter) Write(b []byte) (int, error) {
-	n, err := w.buffer.Write(b)
-	if err != nil {
-		w.logger.WithError(err).Error("Could not write to output buffer.")
-		return n, err
-	}
-	if w.printer != nil {
-		if _, err = w.printer.Write(b); err != nil {
-			w.logger.WithError(err).Warn("Terminal output pipe broke. Printed output may be incomplete.")
-		}
-	}
-	return n, nil
+	return stdoutBuffer.Bytes(), stderrBuffer.Bytes(), nil
 }
