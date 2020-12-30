ecec0a2b056bac020a01906502137640c1b81acb
Duco van Amstel <helcaraxan@gmail.com>
2020-12-30 22:46:07 +0000

    Update direct dependencies

--- internal/util/fileutil.go
+++ internal/util/fileutil.go
@@ -10,20 +10,13 @@ import (
 
 // PrepareOutputPath ensures that the directory containing the specified path exist. It also checks
 // that the full path does not refer to an existing file or directory. If such a path already exists
-// an error is returned unless the `force` parameter is set to `true` in which case we delete it.
-func PrepareOutputPath(log *zap.Logger, outputPath string, force bool) (*os.File, error) {
+// an error is returned.
+func PrepareOutputPath(log *zap.Logger, outputPath string) (*os.File, error) {
 	log = log.With(zap.String("output-path", outputPath))
 	log.Debug("Preparing output path.")
 
 	// Perform target file sanity checks.
 	var sanityCheckErr error
-	if force {
-		log.Debug("Attempting to delete any pre-existing folder or file.")
-		if err := os.RemoveAll(outputPath); err != nil {
-			log.Error("Could not clear existing file.", zap.Error(err))
-			sanityCheckErr = fmt.Errorf("could not remove %q", outputPath)
-		}
-	}
 	if _, err := os.Stat(outputPath); err == nil {
 		log.Error("The specified output path already exists.")
 		sanityCheckErr = fmt.Errorf("target file %q already exists", outputPath)
